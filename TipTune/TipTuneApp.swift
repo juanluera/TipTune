d83ec623fba924b76b4a9439a54988245104452f
Juan <68251336+El-Guapo2024@users.noreply.github.com>
2024-06-27 14:15:05 -0500

    Initial Commit

---
+++ TipTune/TipTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipTuneApp.swift
+//  TipTune
+//
+//  Created by Juan Angel Luera on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TipTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
