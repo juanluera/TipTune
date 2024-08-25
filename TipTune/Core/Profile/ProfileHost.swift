212a2958975f3d3d5ecfb095b26d3319215e2970
Juan <68251336+El-Guapo2024@users.noreply.github.com>
2024-08-25 16:48:52 -0500

    Fixed bugs when fetching profile

---
+++ TipTune/Core/Profile/ProfileHost.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHost.swift
+//  TipTune
+//
+//  Created by Juan Angel Luera on 8/24/24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
