354a3986cdbf4368ecc3f13e5b2e02c0a623ef39
juanluera <angelluera00@gmail.com>
2024-09-08 15:00:23 -0400

    Implemented Forgot Password

---
+++ TipTune/Components/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  TipTune
+//
+//  Created by Juan Angel Luera on 9/8/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ TipTune/Core/Root/NavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationView.swift
+//  TipTune
+//
+//  Created by Juan Angel Luera on 9/5/24.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationView()
+}
